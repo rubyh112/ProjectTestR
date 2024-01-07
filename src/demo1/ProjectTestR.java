6394e99f8648cb29b717818fe734056579982840
abbas <abbas@10.0.0.76>
2024-01-07 13:29:42 -0600

    commit initiated

---
+++ src/demo1/ProjectTestR.java
@@ -0,0 +1,18 @@
+package demo1;
+
+import java.util.ArrayList;
+
+public class ProjectTestR {
+	public static void main(String[] args) {
+
+		ArrayList<String> str1 = new ArrayList<String>();
+		// add element to the ArryList
+		str1.add("z");
+		str1.add("y");
+		str1.add("s");
+		str1.add("r");
+		System.out.println(str1);
+
+	}
+
+}
